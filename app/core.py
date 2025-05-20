837d347e47b53798273c25e599ccc450cc14a139
theplusmath <kdhjms@naver.com>
2025-05-20 10:37:32 +0900

    Update core.py

--- app/core.py
+++ app/core.py
@@ -1,14 +1,21 @@
-from sympy import sympify, latex, srepr
+from sympy import latex, srepr
+from sympy.parsing.latex import parse_latex
 import hashlib
+import re
+
+# $~, $$~, \[ \], \(\), \displaystyle 등을 제거
+def clean_latex_wrappers(latex_str):
+    latex_str = re.sub(r"\$\$|\$|\\\[|\\\]|\\\(|\\\)|\\displaystyle", "", latex_str)
+    return latex_str.strip()
 
 def normalize_latex(latex_str):
     try:
-        expr = sympify(latex_str)
+        cleaned = clean_latex_wrappers(latex_str)
+        expr = parse_latex(cleaned)
 
         hash_value = hashlib.sha256(latex_str.encode()).hexdigest()
         formula_tree = srepr(expr)
         keywords = list({type(atom).__name__ for atom in expr.atoms()})
-        original_latex = latex_str
         normalized_latex = latex(expr)
         sympy_expr = srepr(expr)
 
@@ -16,7 +23,7 @@ def normalize_latex(latex_str):
             "hash": hash_value,
             "main_formula_tree": formula_tree,
             "formula_keywords": keywords,
-            "original_latex": original_latex,
+            "original_latex": latex_str,
             "normalized_latex": normalized_latex,
             "sympy_expr": sympy_expr
         }
